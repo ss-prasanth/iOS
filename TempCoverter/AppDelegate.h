908aaf91395e7441618b61dd574a082cc41cdb99
Prasanth Sivanappan <sivanapp@sorrylaugh-lm.home>
2013-08-04 23:39:21 +0530

    Initial Commit

---
+++ TempCoverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TempCoverter
+//
+//  Created by Prasanth Sivanappan on 04/08/13.
+//  Copyright (c) 2013 Prasanth Sivanappan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TempCoverter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TempCoverter
+//
+//  Created by Prasanth Sivanappan on 04/08/13.
+//  Copyright (c) 2013 Prasanth Sivanappan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
